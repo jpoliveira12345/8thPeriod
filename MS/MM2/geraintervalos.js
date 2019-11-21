043552dd577af8ff3f1b703bb7198950e6b3cd0b
João Paulo de Oliveira <joaopaulodeoliveira123@gmail.com>
2019-11-20 22:18:28 -0300

    MS: Created MM2

---
+++ MS/MM2/geraintervalos.js
@@ -0,0 +1,86 @@
+var ADEFAULT = 65539;
+var MDEFAULT = 1073741824;
+var LAMBDA = 10;
+var MULTIPLICADORTEMPO = 1000;
+var aChegada;
+var xNChegada;
+var mChegada;
+var aSaida;
+var xNSaida;
+var mSaida;
+
+function geraChegadaUniforme() {
+    xNChegada = (xNChegada * aChegada) % mChegada;
+    //return xNChegada / mChegada;
+    return parseInt(Math.random()*10);
+}
+
+function geraChegadaConstante() {
+    return $("#constante-chegada").val() | 7;
+}
+
+
+function geraChegadaExponencial() {
+    let U = geraSaidaUniforme();
+    return (-1 / LAMBDA) * log(1 - U);
+}
+
+function geraHoraChegada() {
+    let distribuicaoChegada = $("input[name='tipoChegada']:checked").val();
+    let tempoEntreChegada;
+    aChegada = $("#a-chegada").val() | ADEFAULT;
+    mChegada = $("#m-chegada").val() | MDEFAULT;
+    switch (distribuicaoChegada) {
+        case 'uniforme':
+            tempoEntreChegada = geraChegadaUniforme();
+            break;
+        case 'constante':
+            tempoEntreChegada = geraChegadaConstante();
+            break;
+        case 'exponencial':
+            tempoEntreChegada = geraChegadaExponencial();
+            break;
+        default:
+            let message = "distribuição de chegada não definido";
+            alert(message);
+            throw message;
+    }
+    return tempoEntreChegada * MULTIPLICADORTEMPO;
+}
+
+function geraSaidaUniforme() {
+    xNSaida = (xNSaida * aSaida) % mSaida;
+    return xNSaida / mSaida;
+}
+function geraSaidaConstante() {
+    return $("#constante-saida").val() | 7;
+}
+function geraSaidaExponencial() {
+    let U = geraSaidaUniforme();
+    return (-1 / LAMBDA) * log(1 - U);
+}
+
+function geraHoraSaida() {
+    let distribuicaoSaida = $("input[name='tipoSaida']:checked").val();
+    let tempoEntreSaida;
+    aSaida = $("#a-saida").val() | ADEFAULT;
+    mSaida = $("#m-saida").val() | MDEFAULT;
+    switch (distribuicaoSaida) {
+        case 'uniforme':
+            tempoEntreSaida = geraSaidaUniforme();
+            break;
+        case 'constante':
+            tempoEntreSaida = geraSaidaConstante();
+            break;
+        case 'exponencial':
+            tempoEntreSaida = geraSaidaExponencial();
+            break;
+        default:
+            let message = "distribuição de saida não definido";
+            alert(message);
+            throw message;
+    }
+    tempoTotalServico += tempoEntreSaida * MULTIPLICADORTEMPO;
+    return tempoEntreSaida;
+}
+
